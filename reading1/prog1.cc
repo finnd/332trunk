0898597673279e952e4329738565b7bbe3d30429
finnd <Dan@dans-mbp.dhcp.wustl.edu>
2016-09-01 09:20:00 -0500

    reading 1 exc done

---
+++ reading1/prog1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+/*
+ * Main function: take two numbers and return their product
+ */
+
+
+int main(){
+  int sum = 0;
+  int val = 0;
+
+  while (std::cin >> val){
+    sum += val;
+}
+  std::cout << sum << std::endl; 
+}
